95218428ca93f6b3001adde86e4e5616cd248f5f
joe ye <joe.ye@sageautomation.com>
2024-05-29 17:01:09 +0930

    new data set

--- main_test.py
+++ main_test.py
@@ -1,27 +1,7 @@
-from Trainer import train_model
-import utils
-from monocyte_resnet50 import monocyte_dataset
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
+import pandas as pd
+df = pd.read_csv('datasets\monocyte.csv')
+duplicates = df.duplicated(subset='image_path', keep=False)
 
-
-
-if __name__ == '__main__':
-    fold = 0
-
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-
-    dataset = monocyte_dataset.MonocyteDataset(csv_file="monocyte_resnet50\cleaned_neutrophils.csv", fold=fold, train=True, transform=transform)
-
-    print("Dataset size:", len(dataset))
-
-    first_sample = dataset[0]
-    print("First sample image shape:", first_sample['image'].shape)
-    print("First sample morphology:", first_sample['morphology'])
-
-    for i, sample in enumerate(dataset):
-        print(f"Sample {i}: Image shape {sample['image'].shape}, Morphology {sample['morphology']}")
-        if i == 3:
-            break
+has_duplicates = duplicates.any()
+print("是否存在重复值：", has_duplicates)
+df.to_csv('datasets\patient.csv', index=False)
