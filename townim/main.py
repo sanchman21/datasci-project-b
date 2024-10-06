786217dbd3ad3e8e367da16821085692e87b8fd9
sanchman21 <goelsanchit21@gmail.com>
2024-10-06 16:18:27 +1030

    updated code

--- townim/main.py
+++ townim/main.py
@@ -7,14 +7,14 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, sampler
 import torchvision.transforms as T
-import torchmetrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, auc, roc_curve, confusion_matrix
 from torchvision import models
 from PIL import Image
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 # sys.path.append('/home/tchowdhury/data/code/CMML-v2/townim')
-sys.path.append('./townim')
+sys.path.append('../townim') # running python main.py from the directory the file is located in
 import utils
 from dataset import CustomDataset, NEUTROPHIL_CSV_PATH, MONOCYTE_CSV_PATH
 
@@ -25,7 +25,6 @@ elif torch.backends.mps.is_available(): # if mps is available
     torch.mps.empty_cache() # empty the cache
     device = "mps" # set the device to mps
 else: # otherwise
-    torch.cpu.empty_cache() # empty the cache
     device = "cpu" # set the device to cpu
 print(f"Using device: {device}") # print the device being used
 
@@ -50,22 +49,26 @@ CSV_PATH = NEUTROPHIL_CSV_PATH if data_type == 'neutrophil' else MONOCYTE_CSV_PA
 
 
 # output_dir = f'./models/{data_type}_fold_{args.fold}'
-output_dir = f'./models/{data_type}_fold_{args.fold}'
+output_dir = f'./experiments/{data_type}_fold_{args.fold}'
 output_dir+='_with_TTA' if is_tta else '_without_TTA'
+model_dir = output_dir + "/model"
+figure_dir = output_dir + "/figures"
 
 # Create the output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
+os.makedirs(output_dir + "/model", exist_ok=True)
+os.makedirs(output_dir + "/figures", exist_ok=True)
     
-# shutil.copyfile('./main.py', os.path.join(output_dir, 'main.py'))
-shutil.copyfile('./townim/main.py', os.path.join(output_dir, 'main.py'))
+shutil.copyfile('./main.py', os.path.join(output_dir, 'main.py')) # copying code file used to train the model
 utils.set_random_seed(123)
 
 # Create data loaders
 batch_size = 32
 IMAGE_SIZE = 352
-IMAGENET_MEAN = [0.485, 0.456, 0.406]         # Mean of ImageNet dataset (used for normalization)
-IMAGENET_STD = [0.229, 0.224, 0.225]          # Std of ImageNet dataset (used for normalization)
+# IMAGENET_MEAN = [0.485, 0.456, 0.406]         # Mean of ImageNet dataset (used for normalization)
+# IMAGENET_STD = [0.229, 0.224, 0.225]          # Std of ImageNet dataset (used for normalization)
+IMAGENET_MEAN = [0.5, 0.5, 0.5]
+IMAGENET_STD = [0.5, 0.5, 0.5]
 
 train_transform = T.Compose([
     # T.RandomResizedCrop(IMAGE_SIZE, scale=(0.8, 1.0), ratio=(1.0, 1.0)),
@@ -73,12 +76,6 @@ train_transform = T.Compose([
     T.RandomVerticalFlip(),
     T.RandomRotation(90),
     T.RandomResizedCrop(IMAGE_SIZE, scale=(0.8, 1.0), ratio=(1.0, 1.0)),
-    T.ToTensor(),
-    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-
-test_transform = T.Compose([
-    T.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     T.ToTensor(),
     T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
 ])
@@ -125,12 +122,12 @@ weights = utils.make_weights_for_balanced_classes(train_dataset.labels, device)
 weighted_sampler = sampler.WeightedRandomSampler(weights, len(weights))
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, pin_memory=True, 
                         sampler=weighted_sampler,
-                        num_workers=4, worker_init_fn=utils.worker_init_fn)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4)
+                        num_workers=8, worker_init_fn=utils.worker_init_fn)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=8)
 print("Dataset loaded")
 
-num_classes = len(set(train_dataset.labels))
 # Define the model architecture (ResNet-50 as an example)
+num_classes = len(set(train_dataset.labels))
 print("Model: Resnet50")
 model = models.resnet50(weights='IMAGENET1K_V1')
 hidden_layer_size = 512
@@ -138,8 +135,6 @@ num_ftrs = model.fc.in_features
 model.fc = nn.Sequential(
     nn.Linear(num_ftrs, hidden_layer_size),
     nn.ReLU(),
-    # nn.BatchNorm1d(hidden_layer_size),
-    # nn.Dropout(0.2),
     nn.Linear(hidden_layer_size, num_classes)
 )
 
@@ -147,171 +142,166 @@ model = model.to(device)
 
 # Define loss function and optimizer
 count = torch.bincount(torch.tensor(train_dataset.labels)).to(device)
-class_weight = len(train_dataset.labels) / count # (count*num_classes)
-# class_weight = torch.tensor(
-#     [1.0 / ((acc.item() + 1e-5) * cls_count.item()) for acc, cls_count in zip(val_class_accuracy, count)]
-# ).to(device)
+class_weight = len(train_dataset.labels) / count
 
 print('Loss class weight:', class_weight)
 class_weight = None
-# criterion = nn.CrossEntropyLoss(weight=class_weight).to(device)
-params = list(model.parameters())
-optimizer = optim.SGD(params, lr=1e-3, weight_decay=5e-4, momentum=0.9)
-end_factor = 1e-5/1e-3
-scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1, end_factor=end_factor, total_iters=num_epochs)
+optimizer = optim.SGD(model.parameters(), lr=5e-5, weight_decay=1e-4, momentum=0.9)
 
-accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes, average='weighted').to(device)
-confmat = torchmetrics.ConfusionMatrix(task="multiclass", num_classes=num_classes, normalize='true').to(device)
-class_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes, average=None).to(device)
+# CSV file to store metrics
+metrics_data = []
+
+# Early stopping variables
+patience = 20  # Number of epochs to wait for improvement
+best_val_loss = float('inf')
+best_test_acc = 0.0
+best_epoch = 0
+epochs_no_improve = 0
+early_stop = False
 
 for epoch in range(num_epochs):
-    t = tqdm(enumerate(train_loader, 0), total=len(train_loader), 
-                smoothing=0.9, position=0, leave=True, 
-                desc="Train: Epoch: "+str(epoch+1)+"/"+str(num_epochs))
+    if early_stop:
+        break
+
+    # Training loop
+    t = tqdm(enumerate(train_loader, 0), total=len(train_loader),
+             smoothing=0.9, position=0, leave=True,
+             desc="Train: Epoch: " + str(epoch + 1) + "/" + str(num_epochs))
     model.train()
     running_loss = 0.0
-    
+    all_preds, all_labels = [], []
+
     for i, (inputs, labels) in t:
         inputs = inputs.to(device).float()
         labels = labels.to(device).long()
         optimizer.zero_grad()
         outputs = model(inputs)
-        loss = F.cross_entropy(outputs, labels) # criterion(outputs, labels)
+        loss = F.cross_entropy(outputs, labels)
         loss.backward()
         optimizer.step()
+
         running_loss += loss.item()
         outputs = F.softmax(outputs, dim=-1)
-        train_accuracy = accuracy(outputs, labels)
-        # torchvision.utils.save_image(utils.denormalize(inputs[:30], IMAGENET_MEAN, IMAGENET_STD), 
-        #                             os.path.join(output_dir, f"samples.jpg"), nrow=10, normalize=True, padding=0)
-    
+        preds = outputs.argmax(dim=1).cpu().numpy()
+        all_preds.extend(preds)
+        all_labels.extend(labels.cpu().numpy())
+
     train_loss = running_loss / len(train_loader)
-    train_losses.append(train_loss)
-    
-    train_accuracy = accuracy.compute() 
-    train_accuracies.append(float(train_accuracy))
-    accuracy.reset()
-    
-    # Validation
+    train_accuracy = accuracy_score(all_labels, all_preds)
+    train_precision = precision_score(all_labels, all_preds, average='weighted')
+    train_recall = recall_score(all_labels, all_preds, average='weighted')
+    train_f1 = f1_score(all_labels, all_preds, average='weighted')
+    train_auroc = roc_auc_score(all_labels, all_preds, average='weighted', multi_class='ovo')
+
+    # Validation loop
     model.eval()
-    val_correct = 0
     val_loss = 0.0
-    
+    val_preds, val_labels, val_probs = [], [], []
+
     with torch.no_grad():
-        if not is_tta:
-            t = tqdm(enumerate(val_loader, 0), total=len(val_loader), 
-                    smoothing=0.9, position=0, leave=True, 
-                    desc="Val: Epoch: "+str(epoch+1)+"/"+str(num_epochs))
-            for i, (inputs, labels) in t:
-                inputs, labels = inputs.to(device).float(), labels.to(device).long()
-                outputs = model(inputs)
-                loss = F.cross_entropy(outputs, labels) # criterion(outputs, labels)
-                val_loss += loss.item()
-                outputs = F.softmax(outputs, dim=-1)
-                val_accuracy = accuracy(outputs, labels)
-                confmat.update(outputs, labels)
-                val_class_accuracy = class_accuracy(outputs, labels)
-        else:
-            t = tqdm(enumerate(zip(*test_loaders)), total=len(test_loaders[0]), 
-                    smoothing=0.9, position=0, leave=True, 
-                    desc="Val: Epoch: "+str(epoch+1)+"/"+str(num_epochs))
-            for i, data in t:
-                inputs, labels = torch.cat([img for img,_ in data], dim=0).to(device).float(), data[0][1].to(device).long()
-                outputs = model(inputs)
-                outputs = outputs.reshape(len(data), int(inputs.shape[0]/len(data)), -1).mean(dim=0)
-                loss = F.cross_entropy(outputs, labels) # criterion(outputs, labels)
-                val_loss += loss.item()
-                outputs = F.softmax(outputs, dim=-1)
-                val_accuracy = accuracy(outputs, labels)
-                confmat.update(outputs, labels)
-                val_class_accuracy = class_accuracy(outputs, labels)
-    
-    val_class_accuracy = class_accuracy.compute()   
+        t = tqdm(enumerate(val_loader, 0), total=len(val_loader),
+                 smoothing=0.9, position=0, leave=True,
+                 desc="Val: Epoch: " + str(epoch + 1) + "/" + str(num_epochs))
+        for i, (inputs, labels) in t:
+            inputs, labels = inputs.to(device).float(), labels.to(device).long()
+            outputs = model(inputs)
+            loss = F.cross_entropy(outputs, labels)
+            val_loss += loss.item()
+            outputs = F.softmax(outputs, dim=-1)
+            preds = outputs.argmax(dim=1).cpu().numpy()
+            probs = outputs[:, 1].cpu().numpy()  # Probabilities for class 1
+            val_preds.extend(preds)
+            val_labels.extend(labels.cpu().numpy())
+            val_probs.extend(probs)
 
     val_loss = val_loss / len(val_loader)
-    val_losses.append(val_loss)
-    val_accuracy = accuracy.compute() 
-    val_accuracies.append(float(val_accuracy))
+    val_accuracy = accuracy_score(val_labels, val_preds)
+    val_precision = precision_score(val_labels, val_preds, average='weighted')
+    val_recall = recall_score(val_labels, val_preds, average='weighted')
+    val_f1 = f1_score(val_labels, val_preds, average='weighted')
+    val_auroc = roc_auc_score(val_labels, val_preds, average='weighted', multi_class='ovo')
 
-    test_loss = val_loss # test_loss / len(test_loader)
-    
-    # Calculate metrics for test data
-    test_accuracy = val_accuracy
-    
-    # scheduler
-    scheduler.step()
-    lr_log = f"LR: {optimizer.param_groups[0]['lr']}" # scheduler._last_lr
-    print(lr_log)
-    logs+=lr_log+'\n'
-    
-    # Print and log epoch results
-    train_results = f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Training Accuracy: {train_accuracy}, Validation Loss: {val_loss}, Validation Accuracy: {val_accuracy}"
-    print(train_results)
-    test_results = f"Test Accuracy: {test_accuracy}, Test Loss: {test_loss}"
-    print(test_results)
-    logs+=train_results+'\n'+test_results+'\n'
-    # print(confmat.compute())
-    
-    # Save the model checkpoint
-    torch.save(model.state_dict(), os.path.join(output_dir, f'last.pth'))
-    # torchvision.utils.save_image(utils.denormalize(inputs[:30, :,:,:], IMAGENET_MEAN, IMAGENET_STD), os.path.join(output_dir, f"samples.jpg"), nrow=10, normalize=True, padding=0)
-    
-    if best_test_acc <= test_accuracy and epoch!=0:
-        best_epoch = epoch+1
-        log = f"Improve accuracy from {best_test_acc} to {test_accuracy}"
-        print(log)
-        logs+=log+"\n"
-        best_test_acc = test_accuracy
-        torch.save(model.state_dict(), os.path.join(output_dir, f'best.pth'))
-        
-        # fig, ax = confmat.plot()
+    print(f"Epoch: {epoch+1}, Training Loss: {train_loss}, Validation Loss: {val_loss}, Training Accuracy: {train_accuracy}, Validation Accuracy: {val_accuracy}")
+    # Store metrics in a CSV file
+    metrics_data.append([epoch+1, train_loss, train_accuracy, train_precision, train_recall, train_f1, train_auroc,
+                         val_loss, val_accuracy, val_precision, val_recall, val_f1, val_auroc])
+
+    df = pd.DataFrame(metrics_data, columns=['Epoch', 'Train Loss', 'Train Accuracy', 'Train Precision', 'Train Recall', 'Train F1', 'Train AUROC',
+                                             'Val Loss', 'Val Accuracy', 'Val Precision', 'Val Recall', 'Val F1', 'Val AUROC'])
+    df.to_csv(os.path.join(output_dir, 'metrics.csv'), index=False)
+
+    # Early stopping check
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        epochs_no_improve = 0
+        torch.save(model.state_dict(), os.path.join(model_dir, 'best.pth'))
+    else:
+        epochs_no_improve += 1
+        if epochs_no_improve == patience:
+            early_stop = True
+            break  # Stop training
+
+    # Save confusion matrix and model when validation accuracy improves
+    if best_test_acc <= val_accuracy and epoch != 0:
+        best_epoch = epoch + 1
+        best_test_acc = val_accuracy
+        torch.save(model.state_dict(), os.path.join(model_dir, 'best.pth'))
+
+        # Confusion matrix
+        conf_matrix = confusion_matrix(val_labels, val_preds, normalize='true')
         fig, ax = plt.subplots()
-        confmat_vals = np.around(confmat.compute().cpu().detach().numpy(), 3)
-        im = ax.imshow(confmat_vals)
-
-        # Show all ticks and label them with the respective list entries
+        im = ax.imshow(conf_matrix)
         ax.set_xticks(np.arange(num_classes))
         ax.set_yticks(np.arange(num_classes))
         ax.set_xlabel('Predicted class')
         ax.set_ylabel('True class')
 
-        # Loop over data dimensions and create text annotations.
         for i in range(num_classes):
             for j in range(num_classes):
-                text = ax.text(j, i, confmat_vals[i, j],ha="center", va="center", color="black", fontsize=12)
+                ax.text(j, i, np.around(conf_matrix[i, j], 2), ha="center", va="center", color="black", fontsize=12)
 
-        ax.set_title("Confusion Matrix on Test for best model")
-        fig.savefig(os.path.join(output_dir, "conf_mat_best.png"))
+        ax.set_title("Confusion Matrix on Validation Set (Best Accuracy)")
+        fig.savefig(os.path.join(figure_dir, "conf_mat_best.png"))
         plt.close()
-    
-    # resetting all metrics
-    accuracy.reset(); class_accuracy.reset(); confmat.reset()
-    
-# Save the printed outputs to a log.txt file
-with open(os.path.join(output_dir, 'log.txt'), 'w') as log_file:
-    log_file.write(logs)
-    log_file.write(f'Best test accuracy: {best_test_acc} in epoch {best_epoch}')
+
+# Plot ROC curve after training
+fpr, tpr, _ = roc_curve(val_labels, val_probs, pos_label=1)
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.4f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+
+# Set xticks and yticks with a step size of 0.1
+plt.xticks(np.arange(0.0, 1.1, 0.1))
+plt.yticks(np.arange(0.0, 1.1, 0.1))
+
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.legend(loc="lower right")
+plt.savefig(os.path.join(figure_dir, 'roc_curve.png'))
+plt.close()
 
 # Save the loss and accuracy graphs
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.plot(range(1, num_epochs+1), train_losses, label='Train Loss')
-plt.plot(range(1, num_epochs+1), val_losses, label='Validation Loss')
+plt.plot(df['Epoch'], df['Train Loss'], label='Train Loss')
+plt.plot(df['Epoch'], df['Val Loss'], label='Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
 plt.title('Train and Validation Loss')
 
 plt.subplot(1, 2, 2)
-plt.plot(range(1, num_epochs+1), train_accuracies, label='Train Accuracy')
-plt.plot(range(1, num_epochs+1), val_accuracies, label='Validation Accuracy')
+plt.plot(df['Epoch'], df['Train Accuracy'], label='Train Accuracy')
+plt.plot(df['Epoch'], df['Val Accuracy'], label='Validation Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.title('Train and Validation Accuracy')
 
-plt.savefig(os.path.join(output_dir, 'loss_accuracy_graph.png'))
+plt.savefig(os.path.join(figure_dir, 'loss_accuracy_graph.png'))
 plt.close()
-
-print("CMML classifier model completed")
-print("Model saved location :", output_dir)
---
+++ townim/summarise_results.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+
+root = "./experiments"
+with_TTA = True
+is_neutrophil = True
+data_type = "neutrophil" if is_neutrophil else "monocyte"
+results = pd.DataFrame(columns=["Fold", "Loss", "Accuracy", "Precision", "Recall", "F1", "AUROC"])
+
+for set_id in range(5):
+    if with_TTA:
+        metrics_path = root + f"/{data_type}_fold_{set_id}_with_TTA/metrics.csv"
+    else:
+        metrics_path = root + f"/{data_type}_fold_{set_id}/metrics.csv"
+    df = pd.read_csv(metrics_path)
+    new_df = pd.Series({
+        "Fold": set_id,
+        "Loss": df["Val Loss"].iloc[-1],
+        "Accuracy": df["Val Accuracy"].iloc[-1],
+        "Precision": df["Val Precision"].iloc[-1],
+        "Recall": df["Val Recall"].iloc[-1],
+        "F1": df["Val F1"].iloc[-1],
+        "AUROC": df["Val AUROC"].iloc[-1]
+    })
+    results = pd.concat([results, new_df.to_frame().T], ignore_index=True)
+
+results.to_csv(root + "/results.csv", index=False)
