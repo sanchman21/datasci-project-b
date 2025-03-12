70e043033d2e0080c98094270d84408e1cc2caf5
Sanchit Goel <sanchitgoel@Sanchits-MacBook-Pro.local>
2025-03-12 17:07:10 +1030

    updated analysis

--- townim/analysis.py
+++ townim/analysis.py
@@ -4,6 +4,11 @@ monocyte_path = '../datasets/monocyte_reassigned.csv'
 monocyte = pd.read_csv(monocyte_path)
 
 rechecked_patient_ids = [2209722160, 2209801259, 2209801421, 2209802027, 2209802125, 2209801848]
-fold3_val_cmml = monocyte[(monocyte['set3'] == 'val') & (monocyte['morphology'] == 0)]['patient_id'].unique()
-fold3_val_cmml = [x for x in fold3_val_cmml if x not in rechecked_patient_ids]
-print(fold3_val_cmml)
+fold3_train_cmml = monocyte[(monocyte['set3'] == 'train') & (monocyte['morphology'] == 0)]['patient_id'].unique().tolist()
+fold0_train_cmml = monocyte[(monocyte['set0'] == 'train') & (monocyte['morphology'] == 0)]['patient_id'].unique().tolist()
+fold1_train_cmml = monocyte[(monocyte['set1'] == 'train') & (monocyte['morphology'] == 0)]['patient_id'].unique().tolist()
+fold2_train_cmml = monocyte[(monocyte['set2'] == 'train') & (monocyte['morphology'] == 0)]['patient_id'].unique().tolist()
+fold4_train_cmml = monocyte[(monocyte['set4'] == 'train') & (monocyte['morphology'] == 0)]['patient_id'].unique().tolist()
+combined_train_cmml = fold0_train_cmml + fold1_train_cmml + fold4_train_cmml + rechecked_patient_ids
+unique_to_fold3 = [x for x in fold3_train_cmml if x not in combined_train_cmml]
+print(unique_to_fold3)
